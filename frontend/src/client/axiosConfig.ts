47749f8b638015a4f0cf8ad0296f06cb59e0c15e
DevilAr69 <131570647+DevilAr69@users.noreply.github.com>
2024-09-05 12:55:50 +0530

    bkend changes

--- frontend/src/client/axiosConfig.ts
+++ frontend/src/client/axiosConfig.ts
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
   baseURL: process.env.NODE_ENV === 'production'
     ? 'https://task-2-asme.onrender.com/' // Replace with your actual backend URL on Render
     : 'http://localhost:5000', // Local development backend URL
-  timeout: 5000, // Timeout in milliseconds
+  timeout: 10000, // Timeout in milliseconds
   headers: {
     'Content-Type': 'application/json',
   },
